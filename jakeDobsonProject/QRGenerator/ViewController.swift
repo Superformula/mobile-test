6b53b32ae95e3cf4f3e04c4b45992abfa9cbd4b4
Jacob Dobson <jake.dobson@gmail.com>
2018-08-23 08:38:06 -0700

    refactored request and image transforms into separate functions

--- jakeDobsonProject/QRGenerator/ViewController.swift
+++ jakeDobsonProject/QRGenerator/ViewController.swift
@@ -8,11 +8,13 @@ import Alamofire
 
 class ViewController: UIViewController {
 //MARK: globals
+	//constants
+	let seedURL = "https://8mpaf1q1g5.execute-api.us-west-1.amazonaws.com/default/random-qr-seed_seed"
+	let headers: HTTPHeaders = ["x-api-key"	: "VVTUTAdalB55yRKQzsM7u6RTowrcUUhJLA82hoN6",
+								  "Accept"	: "application/json"]
+	//vars
+	var apiKey: String?
 	var QRCodeImg: CIImage!
-	let headers: HTTPHeaders = [
-		"x-api-key" : "VVTUTAdalB55yRKQzsM7u6RTowrcUUhJLA82hoN6",
-		"Accept"	: "application/json"
-	]
 //MARK: outlets
 	@IBOutlet weak var generateQRCodeButton: UIButton!
 	@IBOutlet weak var QRCodeImageView: UIImageView!
@@ -23,8 +25,34 @@ class ViewController: UIViewController {
 		QRCodeImageView.layer.borderColor = UIColor.gray.cgColor
 	}
 //MARK: helpers
+	//api request
+	func requestSeed(completion: @escaping (String?, Error?) -> ()) -> () {
+		Alamofire.request(seedURL, headers: headers).validate().responseJSON { response in
+			if response.result.isSuccess {
+				if let jsonDict = response.result.value as?  [String : String] {
+					let seed = (jsonDict["seed"]!)
+					let error = response.error
+					completion(seed, error)
+				}
+			}
+		}
+	}
+	//setup CIImage from seed as qr code...return as UIImage
+	func createCoreImage(seed: String) {
+		let strSeed = seed.data(using: String.Encoding.isoLatin1, allowLossyConversion: false)
+		let filter  = CIFilter(name: "CIQRCodeGenerator")
+		//set filter value for strSeed to convert to QRCode --> "inputMessage"
+		filter?.setValue(strSeed, forKey: "inputMessage")
+		//set value for amount of data entered into each QRCode image --> higher % => larger output
+		//L => 7%    M => 15%    Q => 25    H => 30%
+		filter?.setValue("Q", forKey: "inputCorrectionLevel")
+		//set output image...convert ciImage => UIImage
+		self.QRCodeImg = filter?.outputImage
+		self.QRCodeImageView.image = UIImage(ciImage: self.QRCodeImg)
+		transformQRCodeImg(img: self.QRCodeImageView.image!)
+	}
 	//transform qr code image so is no longer blurry
-	func displayQRCodeImg() {
+	func transformQRCodeImg(img: UIImage) {
 		//scale factor for each axis of QRCode image
 		let scaleX = QRCodeImageView.frame.size.width  / QRCodeImg.extent.size.width
 		let scaleY = QRCodeImageView.frame.size.height / QRCodeImg.extent.size.height
@@ -32,6 +60,21 @@ class ViewController: UIViewController {
 		let transformedQRImg = QRCodeImg.transformed(by: CGAffineTransform(scaleX: scaleX, y: scaleY))
 		//serve newly created & transformed image to QR code image view
 		QRCodeImageView.image = UIImage(ciImage: transformedQRImg)
+//		return QRCodeImageView.image!
+	}
+	//reset image view and generate button
+	func resetButtonAndImg() {
+		//set image and imageView to nil and return/keep button title to "Generate"
+		QRCodeImageView.image = nil
+		QRCodeImageView.layer.borderColor = UIColor.lightGray.cgColor
+		QRCodeImg = nil
+		generateQRCodeButton.setTitle("Generate", for: UIControlState.normal)
+		generateQRCodeButton.backgroundColor = UIColor.blue
+	}
+	//set clear state of button
+	func clearButtonState() {
+		generateQRCodeButton.setTitle("CLEAR", for: UIControlState.normal)
+		generateQRCodeButton.backgroundColor = UIColor.red
 	}
 //MARK: actions
 	//create/remove qr code when user clicks generate/clear button
@@ -39,39 +82,17 @@ class ViewController: UIViewController {
 		//if no QRCode has been generated...get on with the chlorophyl!
 		if QRCodeImg == nil {
 			QRCodeImageView.layer.borderColor = UIColor.clear.cgColor
-			Alamofire.request("https://8mpaf1q1g5.execute-api.us-west-1.amazonaws.com/default/random-qr-seed_seed",
-							  headers: headers).validate().responseJSON { response in
-								if response.result.isSuccess {
-									if let json = response.result.value as? [String : String] {
-										let seed = (json["seed"]!)
-										//need to encode String(seed) as a "rawValue" to play nicely with the value for CIFilter
-										let data = seed.data(using: String.Encoding.isoLatin1, allowLossyConversion: false)
-										let filter = CIFilter(name: "CIQRCodeGenerator")
-										//set filter value for data to convert to QRCode --> "inputMessage"
-										filter?.setValue(data, forKey: "inputMessage")
-										//set value for amount of data entered into each QRCode image --> higher % => larger output
-										//L => 7%    M => 15%    Q => 25    H => 30%
-										filter?.setValue("M", forKey: "inputCorrectionLevel")
-										//set output image...convert ciImage => UIImage
-										self.QRCodeImg = filter?.outputImage
-										self.QRCodeImageView.image = UIImage(ciImage: self.QRCodeImg)
-										//call custom func to transform image so it's not blurry...
-										self.displayQRCodeImg()
-									}
-								} else if response.result.isFailure {
-									print("FAILURE: \(String(describing: response.error))")
-								}
+			requestSeed { (seed, error) in
+				if seed != nil {
+					self.createCoreImage(seed: seed!)
+				} else {
+					print("FAILURE: \(String(describing: error))")
+					self.resetButtonAndImg()
+				}
 			}
-			//clearing QRCodeImage
-			generateQRCodeButton.setTitle("CLEAR", for: UIControlState.normal)
-			generateQRCodeButton.backgroundColor = UIColor.red
+			clearButtonState()
 		} else {
-			//set image and imageView to nil and return/keep button title to "Generate"
-			QRCodeImageView.image = nil
-			QRCodeImageView.layer.borderColor = UIColor.lightGray.cgColor
-			QRCodeImg = nil
-			generateQRCodeButton.setTitle("Generate", for: UIControlState.normal)
-			generateQRCodeButton.backgroundColor = UIColor.blue
+			resetButtonAndImg()
 		}
 	}
 }
