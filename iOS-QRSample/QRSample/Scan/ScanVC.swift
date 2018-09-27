1b6488eb9e04a37abeeaab9e29ce8f29efa346d6
Lamar Jay Caaddfiir <lamar@appnaq.com>
2018-09-26 19:05:59 -0700

    Config Scan VC to detect QR Codes

--- iOS-QRSample/QRSample/Scan/ScanVC.swift
+++ iOS-QRSample/QRSample/Scan/ScanVC.swift
@@ -7,25 +7,52 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ScanVC: UIViewController {
-
+class ScanVC: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+    
     //MARK: - Properties
-    @IBOutlet weak var seedLabel: UILabel!
     @IBOutlet weak var statusLabel: UILabel!
     @IBOutlet weak var containerView: UIView!
-
+    
+    private var captureSession = AVCaptureSession()
+    private var videoPreviewLayer: AVCaptureVideoPreviewLayer?
+    
     //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setupView()
         self.fetchData()
     }
-
+    
     //MARK: - Setup
     private func setupView() {
-        
+        super.viewDidLoad()
+        let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInDualCamera], mediaType: AVMediaType.video, position: .back)
+        guard let captureDevice = deviceDiscoverySession.devices.first else {
+            print("Failed to find camera device")
+            return
+        }
+        do {
+            let input = try AVCaptureDeviceInput(device: captureDevice)
+            self.captureSession.addInput(input)
+            let captureMetadataOutput = AVCaptureMetadataOutput()
+            captureMetadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            self.captureSession.addOutput(captureMetadataOutput)
+            captureMetadataOutput.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
+        } catch {
+            print(error)
+            return
+        }
+        self.videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        self.videoPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        self.videoPreviewLayer?.frame = view.layer.bounds
+        self.view.layer.addSublayer(self.videoPreviewLayer!)
+        self.captureSession.startRunning()
+        self.view.bringSubviewToFront(self.statusLabel)
+        self.view.bringSubviewToFront(self.containerView)
     }
+    
     //MARK: - Fetch
     private func fetchData() {
         
@@ -40,7 +67,7 @@ class ScanVC: UIViewController {
     @IBAction func onTouchShareButton(_ sender: UIButton) {
         
     }
-
+    
     func shareTextButton(text: String) {
         let text = "This is some text that I want to share."
         let textToShare = [ text ]
@@ -57,4 +84,28 @@ class ScanVC: UIViewController {
         activityViewController.excludedActivityTypes = [ .airDrop, .postToFacebook, .mail, .message ]
         self.present(activityViewController, animated: true, completion: nil)
     }
+    
+    
+    //MARK: - AVCaptureMetadataOutputObjectsDelegate
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        if metadataObjects.count < 1 {
+            self.containerView.frame = CGRect.zero
+            self.statusLabel.text = "No QR code is detected"
+            return
+        }
+        let metadataObj = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
+        let barCodeObject = videoPreviewLayer?.transformedMetadataObject(for: metadataObj)
+        self.containerView.frame = barCodeObject!.bounds
+        if let urlString = metadataObj.stringValue {
+            self.displayAlertController(withAlertStyle: .alert, withTitle: "", withMesssage: "Show Web Site?", withActions: [UIAlertAction(title: "Yes", style: .default, handler: { action in
+                if let url = URL(string: urlString) {
+                    if UIApplication.shared.canOpenURL(url) {
+                        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                    }
+                }
+            }), UIAlertAction(title: "No", style: .cancel, handler: nil)])
+            self.statusLabel.text = metadataObj.stringValue
+        }
+    }
+    
 }
