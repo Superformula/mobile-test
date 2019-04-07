cb1c08d900f2bfcc6886502a01d298b23595205f
Michael Maitlen <mgmaitlen@gmail.com>
2019-04-07 14:18:15 -0700

    add firebase http function to fetch seed

---
+++ firebase/functions/src/index.ts
@@ -0,0 +1,14 @@
+import * as functions from 'firebase-functions';
+
+
+const uuidv1 = require('uuid/v1');
+
+export const fetchSeed = functions.https.onRequest((request, response) => {
+
+    const milliseconds = (new Date).getTime();
+
+    return response
+            .status(200)
+            .type("application/json")
+            .send(JSON.stringify({seed: "seed333-" + uuidv1(), expires_at: milliseconds}));
+});
