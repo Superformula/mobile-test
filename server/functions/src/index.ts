93bd6d139029eb73f21369a5fc824694675ecb13
rzornes <russell.zornes@gmail.com>
2019-07-30 17:21:46 -0400

    Implemented seed method with passing test.
    Deployed to cloud.

--- server/functions/src/index.ts
+++ server/functions/src/index.ts
@@ -1,5 +1,40 @@
+import * as crypto from 'crypto';
 import * as functions from 'firebase-functions';
+import * as moment from 'moment';
 
-export const seed = functions.https.onRequest((request, response) => {
-    response.send("Hello from Firebase!");
+// paths:
+//   /seed:
+//     get:
+//       description: Get a seed that can be used to generate a QR code
+//       responses:
+//         '200':
+//           description: seed generated
+//           content:
+//             application/json:
+//               schema:
+//                 $ref: '#/components/schemas/Seed'
+// components:
+//   schemas:
+//     Seed:
+//       type: object
+//       properties:
+//         seed:
+//           type: string
+//           example: d43397d129c3de9e4b6c3974c1c16d1f
+//         expires_at:
+//           type: dateTime
+//           description: ISO date-time
+//           example: '1979-11-12T13:10:42.24Z'
+//
+export const seed = functions.https.onRequest((_req, res) => {
+    // Docs don't specify a length but the example is 32 chars or 16 bytes.
+    const randomString = crypto.randomBytes(16).toString('hex')
+
+    // Docs don't specify a TTL so we'll expire 1 minute from now for easy testing.
+    const expiresAt = moment.utc().add(1, "minute");
+
+    res.contentType("application/json")
+    res.send(JSON.stringify({ seed: randomString, expires_at: expiresAt.toISOString() }))
+    res.status(200)
+    res.end()
 });
--- server/functions/test/index.test.js
+++ server/functions/test/index.test.js
@@ -1,6 +1,7 @@
 const test = require('firebase-functions-test')();
 const chai = require('chai');
 const assert = chai.assert;
+const moment = require('moment');
 
 // Our cloud functions.
 const functions = require('../lib/index');
@@ -12,18 +13,40 @@ describe('Cloud Functions', () => {
     });
 
     describe('seed', () => {
-        it('should return a random seed value', (done) => {
-            const request = {};
-            const response = {
-                send: (value) => {
-                    assert.equal(value, "Hello from Firebase!");
+        it('should return a well formed seed value', (done) => {
+            const req = {};
+            const res = {
+                contentType: (contentType) => {
+                    assert.equal(contentType, "application/json");
+                },
+                send: (bodyText) => {
+                    assert.isNotNull(bodyText, "Missing bodyText");
+
+                    // This will throw if bodyText is not well formed json.
+                    var data = JSON.parse(bodyText)
+
+                    // Check that there is a 'seed' element in the json and its value has a length of 32 characters.
+                    assert.isDefined(data.seed, "JSON missing 'seed' element");
+                    assert.lengthOf(data.seed, 32, "Seed string should be 32 characters in length!")
+
+                    // Check that there is an 'expires_at' element in the json and its value is not empty.
+                    assert.isDefined(data.expires_at, "JSON missing 'expires_at' element");
+                    assert.isNotEmpty(data.expires_at, "Empty expires_at value!")
+
+                    // Check that the 'expires_at' value is a valid ISO_8601 date.
+                    var isValidDate = moment(data.expires_at, moment.ISO_8601, true).isValid()
+                    assert.isTrue(isValidDate, "Invalid expires_at value!"); // true
+                },
+                status: (statuscode) => {
+                    assert.equal(statuscode, 200);
+                },
+                end: () => {
                     done();
-                }
+                },
             };
 
-            // Invoke seed with our fake request and response objects.
-            functions.seed(request, response);
+            // Invoke seed with our fake req and response objects.
+            functions.seed(req, res);
         });
     });
 });
-
