b94737e22ae4bba1003e9f46e31e043c4f9b79f1
rzornes <russell.zornes@gmail.com>
2019-07-31 05:23:09 -0400

    Fixed unit test to work with localized app.

--- app/test/widget_test.dart
+++ app/test/widget_test.dart
@@ -7,13 +7,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:qrgenerator/main.dart';
+import 'package:qrgenerator/app/application.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(buildApp());
+    await tester.pumpAndSettle();
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
