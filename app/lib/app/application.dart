3f7a86a1d1da89be14bb018a1b02e39fa7161b6a
rzornes <russell.zornes@gmail.com>
2019-07-31 04:50:05 -0400

    Move app and home page into their own files.
    Hook up app to localization support.

---
+++ app/lib/app/application.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:qrgenerator/app/intl/app_strings.dart';
+import 'package:qrgenerator/app/ui/home/home.dart';
+
+Widget buildApp() {
+  return Application();
+}
+
+class Application extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // Don't show the debug banner.
+      debugShowCheckedModeBanner: false,
+
+      // MaterialApp creates a Localizations widget with the specified delegates.
+      // AppStrings.of() will find the app's Localizations widget if its context is
+      // a child of the app.
+      // Usage: To access localized strings in UI code...
+      // final strings = AppStrings.of(context);
+      // strings.someStringName;
+      onGenerateTitle: (BuildContext context) {
+        return AppStrings.of(context).appName;
+      },
+      localizationsDelegates: AppStrings.localizationsDelegates,
+      supportedLocales: AppStrings.supportedLocales,
+      localeResolutionCallback: AppStrings.localizationCallback,
+
+      home: buildHomePage(),
+    );
+  }
+}
---
+++ app/lib/app/ui/home/home.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:qrgenerator/app/intl/app_strings.dart';
+
+Widget buildHomePage() {
+  return HomePage();
+}
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final strings = AppStrings.of(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(strings.homeTitle),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('You have pushed the button this many times:'),
+            Text('$_counter', style: Theme.of(context).textTheme.display1),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
