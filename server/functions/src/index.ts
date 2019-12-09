d63673bb85fd637c23fb7501a0c99d3656edb690
Joshua Bancroft <joshua.bancroft@elevationmobile.com>
2019-12-09 17:41:38 -0600

    Add very basic/minimal seed endpoint.

--- server/functions/src/index.ts
+++ server/functions/src/index.ts
@@ -1,8 +1,29 @@
 import * as functions from 'firebase-functions';
+import * as crypto from 'crypto';
+import * as moment from "moment";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const RESPONSE_STATUS_CODE = 200;
+const RESPONSE_CONTENT_TYPE = "application/json";
+
+const SEED_ENCODING = "hex";
+const SEED_LENGTH = 32;
+const SEED_TTL = 1;
+const SEED_TTL_UNIT = "minute";
+
+export const seed = functions.https.onRequest(
+    (_req, res) => {
+        res.contentType(RESPONSE_CONTENT_TYPE);
+
+        res.send(
+            JSON.stringify({
+                    seed: crypto.randomBytes(SEED_LENGTH).toString(SEED_ENCODING),
+                    expires_at: moment.utc().add(SEED_TTL, SEED_TTL_UNIT).toISOString()
+                }
+            )
+        );
+
+        res.status(RESPONSE_STATUS_CODE);
+
+        res.end();
+    }
+);
