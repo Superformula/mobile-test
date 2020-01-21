71871f0d207c31ad7c647c42cf7a37f688824498
Chris Grey <chris@wingtip.dev>
2020-01-21 16:56:30 -0500

    Added basic UI tests for initial screen

--- sftest_flutter/lib/main.dart
+++ sftest_flutter/lib/main.dart
@@ -66,7 +66,7 @@ class _MyHomePageState extends State<MyHomePage> {
           children: <Widget>[
             Text(
               "Select a function from the menu.",
-            ), 
+            ),
           ],
         ),
       ),
--- sftest_flutter/test/widget_test.dart
+++ sftest_flutter/test/widget_test.dart
@@ -8,23 +8,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+
 import 'package:sftest_flutter/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+  testWidgets('Initial UI component validation', (WidgetTester tester) async {
     await tester.pumpWidget(MyApp());
+    expect(find.byType(SpeedDial), findsOneWidget);
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    await tester.tap(find.byType(SpeedDial));
   });
 }
