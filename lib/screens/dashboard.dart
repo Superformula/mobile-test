66498fb85af7799899a66e6940a7678243c5afba
Benjamin Greijn-Lautier <bcgreijnlautier@gmail.com>
2020-12-26 19:11:06 -0500

    feat: home screen with speed dial widget

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
 class Dashboard extends StatelessWidget {
   static const route = '/';
@@ -7,7 +8,40 @@ class Dashboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.all(20),
+          alignment: Alignment.center,
+          child: Text(
+            'Select an option from the bottom right to scan or generate a QR code.',
+            style: Theme.of(context).textTheme.headline6,
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+      floatingActionButton: SpeedDial(
+        children: [
+          SpeedDialChild(
+            label: 'QR Code',
+            child: Icon(
+              Icons.qr_code,
+              color: Colors.white,
+            ),
+          ),
+          SpeedDialChild(
+            label: 'Scan',
+            child: Icon(
+              Icons.photo_camera,
+              color: Colors.white,
+            ),
+          ),
+        ],
+        animatedIcon: AnimatedIcons.menu_close,
+        animationSpeed: 80,
+      ),
     );
   }
 }
