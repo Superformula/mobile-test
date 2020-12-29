50a275c1e18abcbcc5f08c77db7ce8e8122820ce
thinkdigitalsoftware <jwhite@thinkdigital.software>
2020-12-29 12:06:04 -0800

    Add validate endpoint. Update server to print IP address

--- mobile_test_server/bin/main.dart
+++ mobile_test_server/bin/main.dart
@@ -2,12 +2,18 @@ import 'package:mobile_test/mobile_test.dart';
 
 Future main() async {
   final app = Application<MobileTestChannel>()
-      ..options.configurationFilePath = "config.yaml"
-      ..options.port = 8888;
+    ..options.configurationFilePath = "config.yaml"
+    ..options.port = 8888;
 
   final count = Platform.numberOfProcessors ~/ 2;
+  String ipAddress = '';
+  if (Platform.isMacOS) {
+     ipAddress =
+        Process.runSync('ipconfig', ['getifaddr', 'en0']).stdout.toString().trim();
+  }
+
   await app.start(numberOfInstances: count > 0 ? count : 1);
 
-  print("Application started on port: ${app.options.port}.");
+  print("Application started on $ipAddress port: ${app.options.port}.");
   print("Use Ctrl-C (SIGINT) to stop running the application.");
 }
--- mobile_test_server/lib/channel.dart
+++ mobile_test_server/lib/channel.dart
@@ -1,47 +1,65 @@
 import 'dart:convert';
 import 'dart:math';
 
+import 'package:mobile_test/model/qr_code.dart';
+
 import 'mobile_test.dart';
 
 const expirationDurationInSeconds = 10;
+const bool testMode = true;
 
 class MobileTestChannel extends ApplicationChannel {
-  /// Initialize services in this method.
-  ///
-  /// Implement this method to initialize services, read values from [options]
-  /// and any other initialization required before constructing [entryPoint].
-  ///
-  /// This method is invoked prior to [entryPoint] being accessed.
+  final _database = {}; // a simulated database for QR storage
   @override
   Future prepare() async {
     logger.onRecord.listen(
         (rec) => print("$rec ${rec.error ?? ""} ${rec.stackTrace ?? ""}"));
   }
 
-  /// Construct the request channel.
-  ///
-  /// Return an instance of some [Controller] that will be the initial receiver
-  /// of all [Request]s.
-  ///
-  /// This method is invoked after [prepare].
   @override
   Controller get entryPoint {
     final router = Router();
 
-    router.route("/seed").linkFunction((request) async {
-      final seed = _getRandomHex();
-      final expirationTime = DateTime.now()
-          .add(const Duration(seconds: expirationDurationInSeconds))
-          .toIso8601String();
-      return Response.ok({'seed': seed, 'expires_at': expirationTime});
-    });
+    router.route('/seed').linkFunction(_seed);
+    router.route('/validate').linkFunction(_validate);
 
     return router;
+  }
+
+  FutureOr<RequestOrResponse> _seed(Request request) async {
+    final seed = _getRandomHex();
+    final expirationTime = DateTime.now()
+        .add(const Duration(seconds: expirationDurationInSeconds));
+    _database[seed] = QrCode(code: seed, expiresAt: expirationTime);
+    return Response.ok(
+        {'seed': seed, 'expires_at': expirationTime.toIso8601String()});
   }
 
   String _getRandomHex() {
     final rand = Random.secure();
     final ints = List<int>.generate(32, (index) => rand.nextInt(256));
     return base64Url.encode(ints);
+  }
+
+  FutureOr<RequestOrResponse> _validate(Request request) async {
+    final code = await request.body.decode();
+    if (testMode) {
+      return Response.ok({'isValid': true, 'testMode': true});
+    }
+    
+    if (code == null) {
+      return Response.badRequest(body: {'code': 'Missing "code" parameter'});
+    }
+
+    bool isValid;
+    if (_database.containsKey(code)) {
+      isValid = true;
+      print('Code is valid');
+    } else {
+      isValid = false;
+      print('Code is invalid');
+    }
+
+    return Response.ok({'isValid': isValid});
   }
 }
