d1cb58a257b8592434d698bbe66d75d7f3044aea
Fenny Varghese <fvarghese@ecomsystems.com>
2021-01-11 14:50:34 -0500

    Modified the home screen to have a FAB that brings up a menu.

--- qr_app/lib/home/home_screen.dart
+++ qr_app/lib/home/home_screen.dart
@@ -1,39 +1,69 @@
 import 'package:flutter/material.dart';
 import 'package:qr_app/routes.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   final String title;
 
   const HomeScreen({Key key, this.title}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  bool _showMenu = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            FlatButton.icon(
-                onPressed: () {
-                  Navigator.pushNamed(context, RouteNames.Scan,
-                      arguments: {'title': 'Scan'});
-                },
-                icon: Icon(Icons.qr_code_scanner),
-                label: Text('Scan')),
-            FlatButton.icon(
-                onPressed: () {
-                  Navigator.pushNamed(context, RouteNames.Qr,
-                      arguments: {'title': 'QR Code'});
-                },
-                icon: Icon(Icons.qr_code),
-                label: Text('QRCode')),
-          ],
+        appBar: AppBar(
+          title: Text(widget.title),
+          centerTitle: true,
         ),
-      ),
-    );
+        floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: () {
+              setState(() {
+                _showMenu = !_showMenu;
+              });
+            }),
+        body: Stack(
+          children: [
+            Container(),
+            AnimatedPositioned(
+                duration: Duration(milliseconds: 300),
+                bottom: _showMenu ? 100 : -150,
+                right: 8,
+                width: 120,
+                child: Container(
+                    child: Column(children: [
+                  FlatButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, RouteNames.Scan,
+                          arguments: {'title': 'Scan'});
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text("Scan"),
+                        Icon(Icons.qr_code_scanner_sharp),
+                      ],
+                    ),
+                  ),
+                  FlatButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, RouteNames.Qr,
+                          arguments: {'title': 'QR Code'});
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text("QRCode"),
+                        Icon(Icons.qr_code_sharp),
+                      ],
+                    ),
+                  )
+                ])))
+          ],
+        ));
   }
 }
