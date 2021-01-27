9f4ddb7c0b1bd76ad85fda38df3f25edb4a6f190
Edgar Froes <edgar.froes@gmail.com>
2021-01-27 17:15:27 -0300

    feat: snackbar code

---
+++ src/lib/app/utils/snackbar.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class SnackbarUtil {
+  /// Shows a generic [Snackbar]
+  static void showGenericSnackbar(
+    BuildContext context,
+    String text, {
+    bool isError = false,
+  }) {
+    Scaffold.of(context).showSnackBar(SnackBar(
+      backgroundColor: isError ? Colors.red : null,
+      content: Text(
+        text,
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 16.0,
+        ),
+      ),
+    ));
+  }
+}
