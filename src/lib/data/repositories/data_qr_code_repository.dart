212d941e985a74443deaee9a26fff27e73f2f38d
Edgar Froes <edgar.froes@gmail.com>
2021-01-28 20:05:18 -0300

    feat: remove mock seed from code and add get seed from api

--- src/lib/data/repositories/data_qr_code_repository.dart
+++ src/lib/data/repositories/data_qr_code_repository.dart
@@ -1,21 +1,37 @@
+import 'dart:convert';
+
+import 'package:qrcodevalidator/data/mappers/qr_code_mapper.dart';
 import 'package:qrcodevalidator/domain/entities/qr_code.dart';
 import 'package:qrcodevalidator/domain/repositories/qr_code_repository.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
 
 class DataQRCodeRepository implements QRCodeRepository {
-  Future<QRCode> getQRCode() {
-    // TODO.
-    return Future.delayed(Duration(seconds: 1), () {
-      return QRCode(
-        seed: '${DateTime.now().millisecondsSinceEpoch}',
-        expiresAt: DateTime.now().add(Duration(seconds: 4)),
-      );
-    });
+  Future<QRCode> getQRCode() async {
+    var apiUrl = env['API_URL'];
+    var apiKey = env['API_KEY'];
+
+    var response = await http.get(
+      '$apiUrl/default/random-qr-seed_seed',
+      headers: {
+        'x-api-key': apiKey,
+      },
+    );
+
+    if (response.statusCode != 200) {
+      throw Exception(
+          'API returned with status code ${response.statusCode} - ${response.reasonPhrase}');
+    }
+
+    var qrCode = QRCodeMapper.fromMap(jsonDecode(response.body));
+
+    return qrCode;
   }
 
   /// Validate QR Code.
   @override
   Future<bool> validateQRCode(QRCode qrCode, DateTime now) {
-    // If needed, this can be changed for server side validation.
+    // If needed, this can be changed to a server side validation.
     return Future.value(qrCode.isValid(now));
   }
 }
--- src/lib/main.dart
+++ src/lib/main.dart
@@ -4,11 +4,14 @@ import 'package:flutter_clean_architecture/flutter_clean_architecture.dart';
 import 'package:qrcodevalidator/app/pages/display_qr_code/display_qr_code_view.dart';
 import 'package:qrcodevalidator/app/pages/home/home_page.dart';
 import 'package:qrcodevalidator/app/pages/scan_qr_code/scan_qr_code_view.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart' as DotEnv;
 
-void main() {
+Future<void> main() async {
   if (kDebugMode) {
     FlutterCleanArchitecture.debugModeOn();
   }
+
+  await DotEnv.load(fileName: ".env");
 
   runApp(MyApp());
 }
