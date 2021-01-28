2ec0cd5c02cc5aa81983ca6e6226f52a573f8fc6
Edgar Froes <edgar.froes@gmail.com>
2021-01-28 19:38:37 -0300

    style: rename countdown widget for better semantics

--- src/lib/app/pages/display_qr_code/display_qr_code_view.dart
+++ src/lib/app/pages/display_qr_code/display_qr_code_view.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_clean_architecture/flutter_clean_architecture.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 import 'package:qrcodevalidator/app/pages/display_qr_code/display_qr_code_controller.dart';
-import 'package:qrcodevalidator/app/widgets/countdown.dart';
+import 'package:qrcodevalidator/app/widgets/countdown_widget.dart';
 import 'package:qrcodevalidator/data/repositories/data_qr_code_repository.dart';
 
 class DisplayQRCodeView extends View {
@@ -24,46 +24,48 @@ class DisplayQRCodeState
         body: Center(
           key: globalKey,
           child: ControlledWidgetBuilder<DisplayGetQRCodeController>(
-              builder: (context, controller) {
-            if (controller.isLoading == true) {
-              return const CircularProgressIndicator();
-            }
+            builder: (context, controller) {
+              if (controller.isLoading == true) {
+                return const CircularProgressIndicator();
+              }
 
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                if (controller.qrCode == null)
-                  GestureDetector(
-                    onTap: controller.getQRCode,
-                    child: Column(
-                      children: [
-                        const Text(
-                          'QR Code not found\ntap to try again',
-                          textAlign: TextAlign.center,
-                        ),
-                        Container(height: 10),
-                        const Icon(Icons.refresh, size: 50),
-                      ],
-                    ),
-                  ),
-                if (controller.qrCode != null)
-                  Card(
-                    elevation: 10,
-                    child: QrImage(
-                      data: controller.qrCodeContent,
-                      version: QrVersions.auto,
-                      size: MediaQuery.of(context).size.width * 0.8,
-                    ),
-                  ),
-                if (controller.remainingTime != null)
-                  Container(
-                      margin: const EdgeInsets.only(
-                        top: 20,
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  if (controller.qrCode == null)
+                    GestureDetector(
+                      onTap: controller.getQRCode,
+                      child: Column(
+                        children: [
+                          const Text(
+                            'QR Code not found\ntap to try again',
+                            textAlign: TextAlign.center,
+                          ),
+                          Container(height: 10),
+                          const Icon(Icons.refresh, size: 50),
+                        ],
                       ),
-                      child: Countdown(remainingTime: controller.remainingTime))
-              ],
-            );
-          }),
+                    ),
+                  if (controller.qrCode != null)
+                    Card(
+                      elevation: 10,
+                      child: QrImage(
+                        data: controller.qrCodeContent,
+                        version: QrVersions.auto,
+                        size: MediaQuery.of(context).size.width * 0.8,
+                      ),
+                    ),
+                  if (controller.remainingTime != null)
+                    Container(
+                        margin: const EdgeInsets.only(
+                          top: 20,
+                        ),
+                        child: CountdownWidget(
+                            remainingTime: controller.remainingTime))
+                ],
+              );
+            },
+          ),
         ),
       );
 }
