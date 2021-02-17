af887563e4440de6efd6520ecc1a9321c7b324ae
Lucas M. Braz <lmonteirobraz@gmail.com>
2021-02-17 14:32:10 +0100

    Add integration tests for ValidationDialog

---
+++ test/integration/validation/validation_dialog_test.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:qr_code/redux/api_middleware.dart';
+import 'package:qr_code/redux/store.dart';
+
+import '../../mocks.dart';
+import '../../widget_tester_extension.dart';
+
+void main() {
+  testWidgets(
+      'GIVEN a valid qr code'
+      'WHEN ValidationDialog is displayed '
+      'THEN it shows a progress indicator '
+      'AND a successful message', (WidgetTester tester) async {
+    final apiClient = ApiClientMock();
+    when(apiClient.validateQrCode('golden')).thenAnswer((_) => Future.value(true));
+
+    final store = createReduxStore(middleware: [ApiMiddleware(apiClient)]);
+    await tester.pumpValidationDialog(codeToValidate: 'golden', store: store);
+
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+
+    await tester.pumpAndSettle();
+
+    expect(find.text("Good job! You've got a valid QR Code"), findsOneWidget);
+  });
+
+  testWidgets(
+      'GIVEN an expired qr code'
+      'WHEN ValidationDialog is displayed '
+      'THEN it shows an error message', (WidgetTester tester) async {
+    final apiClient = ApiClientMock();
+    when(apiClient.validateQrCode('golden')).thenAnswer((_) => Future.value(false));
+
+    final store = createReduxStore(middleware: [ApiMiddleware(apiClient)]);
+    await tester.pumpValidationDialog(codeToValidate: 'golden', store: store);
+    await tester.pumpAndSettle();
+
+    expect(find.text('This QR Code has expired. Try a new one'), findsOneWidget);
+  });
+
+  testWidgets(
+      'GIVEN ValidationDialog is displayed'
+      'WHEN qr code validation fails '
+      'THEN it shows an error message', (WidgetTester tester) async {
+    final apiClient = ApiClientMock();
+    when(apiClient.validateQrCode('golden')).thenAnswer((_) => Future.error(Exception()));
+
+    final store = createReduxStore(middleware: [ApiMiddleware(apiClient)]);
+    await tester.pumpValidationDialog(codeToValidate: 'golden', store: store);
+    await tester.pumpAndSettle();
+
+    expect(find.text('Something wrong happened'), findsOneWidget);
+    expect(find.text('Try again'), findsOneWidget);
+  });
+}
