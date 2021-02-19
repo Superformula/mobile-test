2394f36e1082c141bed1a9b2e51ef3273246b894
bikkin4 <bikkin4@nationwide.com>
2021-02-19 14:32:59 -0500

    add comments

--- lib/src/bloc/qr_code_generator_bloc.dart
+++ lib/src/bloc/qr_code_generator_bloc.dart
@@ -40,6 +40,7 @@ class QRCodeGeneratorBloc {
     }
   }
 
+  //We can create this validation as the backend service if needed
   bool validateQrCodeStatus(String qrCodeData) {
     final List<String> qrCodeDataArray = qrCodeData.split('##');
     final DateTime expiresAtDateTime = formatDatesToCompare(qrCodeDataArray[1]);
