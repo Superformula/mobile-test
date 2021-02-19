8360e851dde311b3574b014b11bb4f9010b7fe5d
bikkin4 <bikkin4@nationwide.com>
2021-02-19 13:30:21 -0500

    Renamed file to another

--- lib/src/services/respository.dart
+++
@@ -1,9 +0,0 @@
-import 'package:mobile_test/src/model/seed.dart';
-
-import 'api_provider.dart';
-
-class SeedRepository {
-  ApiProvider appApiProvider = ApiProvider();
-
-  Future<SeedResponse> fetchSeed() => appApiProvider.fetchSeed();
-}
