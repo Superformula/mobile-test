82e17a25d3d89246d072dcfc1c7c153361d6669c
bikkin4 <bikkin4@nationwide.com>
2021-02-19 12:53:13 -0500

    file that maintains shared styles across app.

---
+++ lib/src/styles/custom_styles.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class CustomStyles {
+  static const TextStyle defaultStyle =
+      TextStyle(fontWeight: FontWeight.w500, fontSize: 18, color: Colors.white);
+}
