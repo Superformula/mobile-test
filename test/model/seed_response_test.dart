01edc476bcf6f274fa841312df5eb81258e48e89
bikkin4 <bikkin4@nationwide.com>
2021-02-19 13:33:43 -0500

    Validate seed data that coming from backend format, values, null checks.

--- test/model/seed_response_test.dart
+++ test/model/seed_response_test.dart
@@ -6,8 +6,15 @@ import 'seed_mock.dart';
 void main() {
   test('Seed Response is correctly parsed', () async {
     final SeedResponse seedResponse = SeedResponse.fromJson(mockSeed);
-    expect(seedResponse.seed.length, 32);
-    expect(seedResponse.seed, 'COLPTD1YYA910YPAGW4WJYCF1GO979ZP');
-    expect(seedResponse.expiresAt, '10-20-2031');
+    expect(seedResponse.seed.length, 6);
+    expect(seedResponse.seed, '2a6abu');
+    expect(seedResponse.expiresAt, '2080-02-18T23:42:30.802Z');
+  });
+
+  test('Seed Response handles null data', () async {
+    final SeedResponse seedResponse = SeedResponse.fromJson(mockSeedNullData);
+    expect(seedResponse.seed?.length, null);
+    expect(seedResponse.seed, null);
+    expect(seedResponse.expiresAt, null);
   });
 }
