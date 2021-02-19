6b845d3c339f4a840887e21095e4a32719a29101
bikkin4 <bikkin4@nationwide.com>
2021-02-19 13:14:18 -0500

    Qrcode bloc test which covers good and bad data from services, validate QR code status.

--- test/bloc/qr_code_generator_bloc_test.dart
+++ test/bloc/qr_code_generator_bloc_test.dart
@@ -1,54 +1,91 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mobile_test/src/bloc/qr_code_generator_bloc.dart';
 import 'package:mobile_test/src/model/seed.dart';
-import 'package:mobile_test/src/services/respository.dart';
 import 'package:mockito/mockito.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 
+import '../mockSetUp/mock_classes.dart';
 import '../model/seed_mock.dart';
-
-class MockSeedRepository extends Mock implements SeedRepository {}
 
 void main() {
   test('throws AssertionError if SeedRepository is null', () {
     expect(
-      () => QRCodeGeneratorBloc(seedRepository: null),
+      () => QRCodeGeneratorBloc(repository: null),
       throwsA(isAssertionError),
     );
   });
 
-  test('load stuff must sink Response', () {
-    MockSeedRepository mockSeedRepository = MockSeedRepository();
+  test('check seed data get added to stream', () async {
+    final MockSeedRepository mockSeedRepository = MockSeedRepository();
     when(mockSeedRepository.fetchSeed()).thenAnswer(
         (_) => Future<SeedResponse>.value(SeedResponse.fromJson(mockSeed)));
     final QRCodeGeneratorBloc qrCodeGeneratorBloc =
-        QRCodeGeneratorBloc(seedRepository: mockSeedRepository);
-    qrCodeGeneratorBloc.qrCodeSeed.listen((response) {
+        QRCodeGeneratorBloc(repository: mockSeedRepository);
+    qrCodeGeneratorBloc.qrCodeSeed$.listen((QrImage response) {
       expect(response, isA<QrImage>());
     });
-    qrCodeGeneratorBloc.qrCodeExpiresAt.listen((response) {
+    qrCodeGeneratorBloc.qrCodeExpiresAt$.listen((String response) {
       expect(response, isA<String>());
     });
-    qrCodeGeneratorBloc.getGenerateQRCode();
+    await qrCodeGeneratorBloc.getGenerateQRCode(
+        seedRepository: mockSeedRepository);
+    expect(
+        qrCodeGeneratorBloc.qrCodeExpiresAt$.value, '2080-02-18 06:42:30.000');
   });
 
-  test('Check if QR code is not expired', () {
-    MockSeedRepository mockSeedRepository = MockSeedRepository();
+  test('null seed data get added to stream without errors', () async {
+    final MockSeedRepository mockSeedRepository = MockSeedRepository();
+    when(mockSeedRepository.fetchSeed()).thenAnswer((_) =>
+        Future<SeedResponse>.value(SeedResponse.fromJson(mockSeedNullData)));
     final QRCodeGeneratorBloc qrCodeGeneratorBloc =
-        QRCodeGeneratorBloc(seedRepository: mockSeedRepository);
+        QRCodeGeneratorBloc(repository: mockSeedRepository);
+    await qrCodeGeneratorBloc.getGenerateQRCode(
+        seedRepository: mockSeedRepository);
+    expect(qrCodeGeneratorBloc.qrCodeExpiresAt$.value, null);
+    expect(qrCodeGeneratorBloc.qrCodeSeed$.value, null);
+  });
+
+  test('validate QR code that is not expired', () {
+    final MockSeedRepository mockSeedRepository = MockSeedRepository();
+    final QRCodeGeneratorBloc qrCodeGeneratorBloc =
+        QRCodeGeneratorBloc(repository: mockSeedRepository);
+    final DateTime parsedDateCurrent =
+        DateTime.now().add(const Duration(seconds: 60));
+
     expect(
         qrCodeGeneratorBloc
-            .checkQrCodeIsValid("COLPTD1YYA910YPAGW4WJYCF1GO979ZP|2031-10-10"),
+            .validateQrCodeStatus('2a6abu##' + parsedDateCurrent.toString()),
         true);
   });
 
-  test('Check if QR code is expired', () {
-    MockSeedRepository mockSeedRepository = MockSeedRepository();
+  test('validate if QR code is expired if dateTime is in the past', () {
+    final MockSeedRepository mockSeedRepository = MockSeedRepository();
     final QRCodeGeneratorBloc qrCodeGeneratorBloc =
-        QRCodeGeneratorBloc(seedRepository: mockSeedRepository);
+        QRCodeGeneratorBloc(repository: mockSeedRepository);
+
     expect(
         qrCodeGeneratorBloc
-            .checkQrCodeIsValid("COLPTD1YYA910YPAGW4WJYCF1GO979ZP|2020-10-10"),
+            .validateQrCodeStatus('2a6abu##2021-02-18T23:42:30.802Z'),
         false);
+  });
+
+  test('validate if QR code is expired if dateTime is at the current moment',
+      () {
+    final MockSeedRepository mockSeedRepository = MockSeedRepository();
+    final QRCodeGeneratorBloc qrCodeGeneratorBloc =
+        QRCodeGeneratorBloc(repository: mockSeedRepository);
+    expect(
+        qrCodeGeneratorBloc
+            .validateQrCodeStatus('2a6abu##' + DateTime.now().toString()),
+        true);
+  });
+
+  test('emits done event to listeners when the subject is closed', () async {
+    final MockSeedRepository mockSeedRepository = MockSeedRepository();
+    final QRCodeGeneratorBloc qrCodeGeneratorBloc =
+        QRCodeGeneratorBloc(repository: mockSeedRepository);
+    qrCodeGeneratorBloc.dispose();
+    expect(qrCodeGeneratorBloc.expiresAtFetcher.isClosed, true);
+    expect(qrCodeGeneratorBloc.seedFetcher.isClosed, true);
   });
 }
