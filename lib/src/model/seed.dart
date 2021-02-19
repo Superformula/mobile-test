d36cb324581bc42cd74756ade1ef60b1f3123d98
bikkin4 <bikkin4@nationwide.com>
2021-02-19 13:30:49 -0500

    model for Qr code seed data with null checks

--- lib/src/model/seed.dart
+++ lib/src/model/seed.dart
@@ -2,10 +2,8 @@ class SeedResponse {
   SeedResponse({this.seed, this.expiresAt});
 
   factory SeedResponse.fromJson(Map<String, dynamic> json) => SeedResponse(
-      seed: json['seed'] == null ? 0.0 : json['seed'],
-      expiresAt: json['expiresAt'] == null
-          ? DateTime.now().toString()
-          : json['expiresAt']);
+      seed: json['seed'] == null ? null : json['seed'],
+      expiresAt: json['expiresAt'] == null ? null : json['expiresAt']);
 
   final String seed;
   final String expiresAt;
