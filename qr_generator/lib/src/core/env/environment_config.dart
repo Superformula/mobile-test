2fb09b07d601debd82d200886d8544a4b0b7aac5
Marcos Sevilla <marcossevilla14@gmail.com>
2021-03-01 15:22:28 -0600

    Added env folder to remote repository

---
+++ qr_generator/lib/src/core/env/environment_config.dart
@@ -0,0 +1,6 @@
+/// Contains all the environment variables
+/// used throughout the project.
+class EnvironmentConfig {
+  /// API service URl
+  static const apiUrl = String.fromEnvironment('apiUrl');
+}
