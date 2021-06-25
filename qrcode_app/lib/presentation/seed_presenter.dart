1cdae8622afbf69c0c00c2be7b64d1a553ffdd74
Fernanda Dias <fernandasd26@gmail.com>
2021-06-24 22:37:26 -0300

    removing comments

--- qrcode_app/lib/presentation/seed_presenter.dart
+++ qrcode_app/lib/presentation/seed_presenter.dart
@@ -51,7 +51,6 @@ class SeedPresenter implements Presenter {
 
   @override
   Future getData() async {
-    // _tickController = StreamController<int>.broadcast();
     _controller.add(LoadingState());
     _seedState.seed = await repository.getSeed();
     update();
@@ -70,26 +69,6 @@ class SeedPresenter implements Presenter {
         .tick(difference.inSeconds)
         .listen((duration) => updateTicker(duration.abs()));
   }
-  // _tickController.add(duration)
-  // tick(DateTime expirationDate) {
-  //   Duration difference = DateTime.now().difference(expirationDate);
-  //   Timer.periodic(Duration(seconds: 1), (timer) {
-  //     print(timer.tick);
-  //     if (difference.inSeconds + timer.tick - 1 <= 0) {
-  //       return _tickController.add(difference.inSeconds + timer.tick - 1);
-  //     }
-  //   });
-  //   // _tickController.addStream(
-  //   //     Stream.periodic(Duration(seconds: 1), (x) {
-  //   //       if (difference.inSeconds + x - 1 < 0)
-  //   //         return difference.inSeconds + x - 1;
-  //   //       else {
-  //   //         _tickController.close();
-  //   //         return 0;
-  //   //       }
-  //   //     }),
-  //   //     cancelOnError: true);
-  // }
 
   Stream<PresenterState> get streamData => _controller.stream;
   Stream<int> get expirationData => _tickController.stream;
