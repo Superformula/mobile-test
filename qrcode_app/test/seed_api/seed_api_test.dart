fde606ba95f7c3e2905e216a1e6e7b6f04f7d2cc
Fernanda Dias <fernandasd26@gmail.com>
2021-06-27 23:13:00 -0300

    creating unit tests for seed api and seed model

---
+++ qrcode_app/test/seed_api/seed_api_test.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:http/http.dart' as http;
+import 'package:mockito/mockito.dart';
+import 'package:qrcode_app/data/datasource/seeds_api_datasource.dart';
+import 'package:qrcode_app/domain/entities/seed.dart';
+
+import 'seed_api_test.mocks.dart';
+
+@GenerateMocks([http.Client])
+void main() {
+  const URL = 'https://seed-server.herokuapp.com/seed';
+  group('fetch seed', () {
+    test('returns a seed if the http call completes', () async {
+      final client = MockClient();
+      when(client.get(Uri.parse(URL))).thenAnswer((_) async => http.Response(
+          '{"seed":"RU-kDEoRh0g=","expiration":"2021-06-27 22:56:20.556149"}',
+          200));
+      expect(await SeedApiDatasource.getSeed(client), isA<Seed>());
+    });
+    test('returns an error if the http call failures', () async {
+      final client = MockClient();
+      when(client.get(Uri.parse(URL)))
+          .thenAnswer((_) async => http.Response('Not Found', 404));
+      expect(await SeedApiDatasource.getSeed(client), throwsException);
+    });
+  });
+}
---
+++ qrcode_app/test/seed_model_test.dart
@@ -0,0 +1,16 @@
+import 'dart:convert';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:qrcode_app/data/models/seed_model.dart';
+import 'package:qrcode_app/domain/entities/seed.dart';
+
+void main() {
+  test('Seed model should parse json and return a Seed entity', () {
+    String fakeJson =
+        '{"seed":"RU-kDEoRh0g=","expiration":"2021-06-27 22:56:20.556149"}';
+
+    SeedModel seedModel = SeedModel();
+    dynamic seed = seedModel.fromJson(json.decode(fakeJson));
+    expect(seed, isA<Seed>());
+  });
+}
