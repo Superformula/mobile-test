eb3a921301a6bbbe34809a2bc32c842f4ec003c3
Fernanda Dias <fernandasd26@gmail.com>
2021-07-06 14:46:35 -0300

    renaming seed model class to SeedDto and working on refactorings

--- qrcode_app/lib/data/models/seed_model.dart
+++ qrcode_app/lib/data/models/seed_model.dart
@@ -1,14 +1,10 @@
-import 'package:qrcode_app/domain/entities/seed.dart';
-
-class SeedModel {
-  late String seed;
-  late DateTime expirationDate;
-
-  Seed fromJson(dynamic json) {
+class SeedDto {
+  String seed = '0123';
+  DateTime expirationDate = DateTime.now().add(Duration(seconds: 10));
+  SeedDto(seed, expirationDate);
+  SeedDto.fromJson(dynamic json) {
     seed = json['seed'] ?? 'default';
-    // expirationDate = DateTime.now().add(Duration(seconds: 10));
     expirationDate = DateTime.tryParse(json['expiration']) ??
-        DateTime.now().add(Duration(hours: 1));
-    return Seed(expirationDate: expirationDate, seed: seed);
+        DateTime.now().add(Duration(seconds: 10));
   }
 }
