0b74cc930ebd2c82820da6884a14a973f41f028b
Fernanda Dias <fernandasd26@gmail.com>
2021-07-18 19:32:57 -0300

    separating layers for abstract repository class

--- qrcode_app/lib/data/repositories/seeds_repository.dart
+++ qrcode_app/lib/data/repositories/seeds_repository.dart
@@ -1,10 +1,7 @@
 import 'package:http/http.dart';
 import 'package:qrcode_app/data/datasource/seeds_api_datasource.dart';
 import 'package:qrcode_app/domain/entities/seed.dart';
-
-abstract class SeedRepository {
-  Future<Seed> getSeed();
-}
+import 'package:qrcode_app/domain/repository/seed_repository.dart';
 
 class SeedRepositoryImpl implements SeedRepository {
   final SeedApiDatasource apiDatasource;
