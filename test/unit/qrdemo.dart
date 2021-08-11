f262fe33af1fd1830b290e6bbce010758dfe55c8
Michael Gallego <gallegom58@gmail.com>
2021-08-11 04:40:17 -0400

    added some unit tests

--- test/unit/qrdemo.dart
+++ test/unit/qrdemo.dart
@@ -1,56 +1,60 @@
+import 'dart:async';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:provider/provider.dart';
+import 'package:qrdemo/providers/qr_provider.dart';
 import 'package:qrdemo/providers/timer_provider.dart';
+import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
+import 'package:uuid/uuid.dart';
 
 void main() async {
   group('qr demo in testing', () {
+    // Reading the documentation, firestore recommends testing using Fakes
     testWidgets('check database exists', (WidgetTester tester) async {
-      await Firebase.initializeApp();
-      final snapshot =
-          FirebaseFirestore.instance.collection('completed').snapshots();
-      expect(snapshot.length, isNotEmpty);
+      final firestore = FakeFirebaseFirestore();
+      final completed = firestore.collection('completed');
+      await completed.add({'code': Uuid().v4(), 'time': DateTime.now()});
+
+      await tester.pumpWidget(StreamBuilder<QuerySnapshot>(
+          stream: completed.snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              expect(snapshot.data!.docs.length, isNotEmpty);
+              return Container();
+            } else {
+              return Container();
+            }
+          }));
     });
 
-    testWidgets('timer started', (WidgetTester tester) async {
-      await tester.pumpWidget(TimerExample());
+    testWidgets('generate code', (WidgetTester tester) async {
+      await tester.pumpWidget(ChangeNotifierProvider(
+          create: (context) => QRProvider(),
+          lazy: false,
+          builder: (context, _) {
+            WidgetsBinding.instance!.addPostFrameCallback((timeStamp) {
+              String code = Provider.of<QRProvider>(context, listen: false)
+                  .generateCode();
+              expect(code, isNotEmpty);
+            });
+            return Container();
+          }));
+    });
+
+    testWidgets('countdown is 15 when reset', (WidgetTester tester) async {
+      await tester.pumpWidget(ChangeNotifierProvider(
+          create: (context) => TimerProvider(),
+          builder: (context, _) {
+            WidgetsBinding.instance!.addPostFrameCallback((timeStamp) {
+              Provider.of<TimerProvider>(context, listen: false).reset();
+              expect(
+                  Provider.of<TimerProvider>(context, listen: false).countdown,
+                  15);
+            });
+            return Container();
+          }));
     });
   });
-}
-
-class TimerExample extends StatefulWidget {
-  const TimerExample({Key? key}) : super(key: key);
-
-  @override
-  _TimerExampleState createState() => _TimerExampleState();
-}
-
-class _TimerExampleState extends State<TimerExample> {
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder<void>(
-        future: Provider.of<TimerProvider>(context, listen: false).startClock(),
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.none:
-            case ConnectionState.active:
-            case ConnectionState.waiting:
-              return Scaffold(
-                body: Container(
-                  child: Center(
-                    child: CircularProgressIndicator(),
-                  ),
-                ),
-              );
-            case ConnectionState.done:
-              return Scaffold(
-                body: Container(
-                  child: Center(),
-                ),
-              );
-          }
-        });
-  }
 }
