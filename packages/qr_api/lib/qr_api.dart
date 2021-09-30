df5bf2d4eef5a3a53578b062e66bc21a16c96920
Enzo Lizama <lizama.enzo@gmail.com>
2021-09-30 17:46:44 -0500

    export result api class

--- packages/qr_api/lib/qr_api.dart
+++ packages/qr_api/lib/qr_api.dart
@@ -1,5 +1,6 @@
 library qr_api;
 
+export 'src/api/api_result.dart';
 export 'src/api/qr_api.dart';
 export 'src/repository/seed_repository.dart';
 export 'src/repository/seed_repository_impl.dart';
