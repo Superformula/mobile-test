ee2b8f6dd0ad9c514ae3d9f9f1571ddabe0cacab
Enzo Lizama <lizama.enzo@gmail.com>
2021-09-30 02:05:35 -0500

    setup dio dependencies

--- lib/app/dependencies.dart
+++ lib/app/dependencies.dart
@@ -1,11 +1,22 @@
+import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:qr_api/qr_api.dart';
 
 GetIt getIt = GetIt.instance;
 
 void setupDependencies() {
+  final options = BaseOptions(
+    baseUrl: 'https://8mpaf1q1g5.execute-api.us-west-1.amazonaws.com',
+    connectTimeout: 5000,
+    receiveTimeout: 3000,
+    headers: <String, dynamic>{
+      'x-api-key': 'VVTUTAdalB55yRKQzsM7u6RTowrcUUhJLA82hoN6'
+    },
+  );
+
   getIt
-    ..registerSingleton<QrApi>(QrApi())
+    ..registerSingleton(Dio(options))
+    ..registerSingleton<QrApi>(QrApi(getIt.get<Dio>()))
     ..registerFactory<SeedRepository>(
         () => SeedRepositoryImpl(getIt.get<QrApi>()));
 }
