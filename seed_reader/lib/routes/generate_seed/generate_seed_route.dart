ca10f8e2cc1c4b5d3ab19c31fad22ce7085530d9
Cristian Madrid <cristianmadridd@gmail.com>
2021-10-05 07:36:59 -0300

    chore: Mock seed interactor and add missing bloc injection

--- seed_reader/lib/routes/generate_seed/generate_seed_route.dart
+++ seed_reader/lib/routes/generate_seed/generate_seed_route.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:provider/provider.dart';
+import 'package:provider/single_child_widget.dart';
 import '../../routes/generate_seed/generate_seed_page.dart';
+import '../../routes/generate_seed/generate_seed_page_bloc.dart';
 import '../app_route.dart';
 
 class GenerateSeedRoute implements AppRoute {
@@ -10,8 +14,20 @@ class GenerateSeedRoute implements AppRoute {
 
   @override
   Route<T> handle<T>(RouteSettings settings) {
+    final GenerateSeedPageBloc bloc = GenerateSeedPageBloc(
+      seedInteractor: GetIt.I(),
+    );
+    bloc.refreshSeed();
+
     return MaterialPageRoute<T>(
-      builder: (_) => const GenerateSeedPage(),
+      builder: (_) => MultiProvider(
+        providers: <SingleChildWidget>[
+          Provider<GenerateSeedPageBloc>(
+            create: (_) => bloc,
+          ),
+        ],
+        child: const GenerateSeedPage(),
+      ),
     );
   }
 }
--- seed_reader/test/routes/generate_seed/generate_seed_route_test.dart
+++ seed_reader/test/routes/generate_seed/generate_seed_route_test.dart
@@ -1,14 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:get_it/get_it.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:provider/provider.dart';
+import 'package:provider/single_child_widget.dart';
+import 'package:seed_reader/interactors/seed_interactor.dart';
+import 'package:seed_reader/models/seed.dart';
 import 'package:seed_reader/routes/generate_seed/generate_seed_page.dart';
+import 'package:seed_reader/routes/generate_seed/generate_seed_page_bloc.dart';
 import 'package:seed_reader/routes/generate_seed/generate_seed_route.dart';
 import '../../common/base_testable_widget.dart';
+import 'generate_seed_route_test.mocks.dart';
 
+late MockSeedInteractor _seedInteractor;
 late GenerateSeedRoute _route;
+@GenerateMocks(<Type>[SeedInteractor])
 void main() {
-  setUp(() {
+  setUp(() async {
+    _seedInteractor = MockSeedInteractor();
+    when(_seedInteractor.fetchSeed()).thenAnswer(
+      (_) => Future<Seed>.value(
+        Seed(
+          expiration: DateTime.now().add(const Duration(seconds: 2)),
+          value: 'foobar',
+        ),
+      ),
+    );
+    await GetIt.I.reset();
+    GetIt.I.registerSingleton<SeedInteractor>(_seedInteractor);
     _route = const GenerateSeedRoute();
   });
+
   testWidgets('#canHandle', (WidgetTester tester) async {
     expect(_route.canHandle('/foo'), false);
     expect(_route.canHandle('/generate_seed'), true);
@@ -17,19 +40,35 @@ void main() {
     final GlobalKey<NavigatorState> navigatorKey = GlobalKey();
 
     await tester.pumpWidget(
-      BaseWidgetTest(
-        navigatorKey: navigatorKey,
+      MultiProvider(
+        providers: <SingleChildWidget>[
+          Provider<GenerateSeedPageBloc>(
+            create: (_) => GenerateSeedPageBloc(
+              seedInteractor: const SeedInteractor(),
+            ),
+          ),
+          StreamProvider<Seed?>(
+            create: (_) => const Stream<Seed?>.empty(),
+            initialData: null,
+          ),
+        ],
+        child: BaseWidgetTest(
+          navigatorKey: navigatorKey,
+        ),
       ),
     );
-    await tester.pumpAndSettle();
+    await tester.pump();
+    await tester.pump();
 
     navigatorKey.currentState?.push<dynamic>(
       _route.handle<dynamic>(
         const RouteSettings(),
       ),
     );
-    await tester.pumpAndSettle();
+    await tester.pump();
+    await tester.pump();
 
     expect(find.byType(GenerateSeedPage), findsOneWidget);
+    verify(_seedInteractor.fetchSeed()).called(1);
   });
 }
