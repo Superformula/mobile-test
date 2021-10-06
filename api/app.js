b8a83f758ad12a238f6221f68f617b9923ae4324
Cristian Madrid <cristianmadridd@gmail.com>
2021-10-06 10:27:08 -0300

    feat: create a simple rest application

---
+++ api/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const { 
+    v4: uuidv4,
+  } = require('uuid');
+
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log('Listenning on port ${port}'));
+
+app.get('/api/seed', (req, res) => {
+    var expirationDate = new Date();
+    expirationDate.setSeconds( expirationDate.getSeconds() + 20 );
+
+    res.send({
+        "seed": uuidv4(),
+        "expires_at": JSON.stringify(expirationDate)
+    });
+});
