d5aa314d3f8c277543cc8576796da76255e9aedb
Cristian Madrid <cristianmadridd@gmail.com>
2021-10-05 22:54:29 -0300

    chore: Add missing mock

--- seed_reader/test/routes/scan_seed/scan_seed_route_test.dart
+++ seed_reader/test/routes/scan_seed/scan_seed_route_test.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:get_it/get_it.dart';
+import 'package:seed_reader/interactors/seed_interactor.dart';
 import 'package:seed_reader/routes/scan_seed/scan_seed_page.dart';
 import 'package:seed_reader/routes/scan_seed/scan_seed_route.dart';
+
 import '../../common/base_testable_widget.dart';
+import '../../mocks/mock_generator.mocks.dart';
 
 late ScanRoute _route;
+late MockSeedInteractor _seedInteractor;
+
 void main() {
-  setUp(() {
+  setUp(() async {
+    _route = const ScanRoute();
+    _seedInteractor = MockSeedInteractor();
+
+    await GetIt.I.reset();
+    GetIt.I.registerSingleton<SeedInteractor>(_seedInteractor);
     _route = const ScanRoute();
   });
   testWidgets('#canHandle', (WidgetTester tester) async {
