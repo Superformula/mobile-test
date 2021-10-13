5cf6f54628c58063a8709c24be7f68ce84b97133
Enzo Lizama <lizama.enzo@gmail.com>
2021-10-13 11:39:30 -0500

    change get it instance to final

--- lib/app/dependencies.dart
+++ lib/app/dependencies.dart
@@ -2,7 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:qr_api/qr_api.dart';
 
-GetIt getIt = GetIt.instance;
+final getIt = GetIt.instance;
 
 void setupDependencies() {
   final options = BaseOptions(
