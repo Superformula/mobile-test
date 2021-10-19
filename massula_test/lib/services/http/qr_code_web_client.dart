92f58b1a76800615c54fdcbc10371434183da038
Matheus Massula <matheusmassula1@gmail.com>
2021-10-19 08:53:35 -0300

    add web client

---
+++ massula_test/lib/services/http/qr_code_web_client.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+import 'package:http_interceptor/http/http.dart';
+import 'package:massula_test/models/remote/qr_code_seed.dart';
+
+import 'interceptors/qr_code_interceptor.dart';
+
+final Client interceptorClient = InterceptedClient.build(
+  interceptors: [QRCodeInterceptor()],
+  requestTimeout: Duration(seconds: 15)
+);
+final String url = 'https://8mpaf1q1g5.execute-api.us-west-1.amazonaws.com/';
+
+class QRCodeWebClient {
+  final Client client;
+
+  QRCodeWebClient(this.client);
+  
+  Future<QRCodeSeed?>? getQRCode() async {
+    final headers = {'x-api-key': 'VVTUTAdalB55yRKQzsM7u6RTowrcUUhJLA82hoN6'};
+
+    final Response response = await client.get(
+      Uri.parse(url + 'default/random-qr-seed_seed'),
+      headers: headers
+    );
+
+    return QRCodeSeed.fromJson(jsonDecode(response.body));
+  }
+}
