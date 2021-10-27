e28ae75e9de37a877da8896ecebc53464d771d69
Matheus Massula <matheusmassula1@gmail.com>
2021-10-27 18:49:24 -0300

    bugFix: using animation instead of set state on build method

--- massula_test/lib/pages/widgets/seconds_count_down.dart
+++ massula_test/lib/pages/widgets/seconds_count_down.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:massula_test/resources/string_constant.dart';
 
-class SecondsCountDown extends StatefulWidget {
+class SecondsCountDown extends StatelessWidget {
   final VoidCallback onEnd;
   final int seconds;
 
@@ -12,32 +12,10 @@ class SecondsCountDown extends StatefulWidget {
   }) : super(key: key);
 
   @override
-  _SecondsCountDownState createState() => _SecondsCountDownState();
-}
-
-class _SecondsCountDownState extends State<SecondsCountDown> {
-  late int currentSecondsLeft;
-
-  @override
-  void initState() {
-    currentSecondsLeft = widget.seconds;
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    rebuildInASecond();
-    return Text('$currentSecondsLeft ${StringConstant.SECONDS_LEFT_LABEL}');
-  }
-
-  void rebuildInASecond() async {
-    if(currentSecondsLeft > 0) {
-      await Future.delayed(Duration(seconds: 1));
-      if(this.mounted) {
-        setState(() => currentSecondsLeft-- );
-      }
-    } else {
-      widget.onEnd();
-    }
-  }
+  Widget build(BuildContext context) => TweenAnimationBuilder<double>(
+    tween: Tween(begin: seconds.toDouble(), end: 0.0),
+    duration: Duration(seconds: seconds),
+    builder: (context, value, child) => Text('${value.toInt()} ${StringConstant.SECONDS_LEFT_LABEL}'),
+    onEnd: () => onEnd(),
+  );
 }
