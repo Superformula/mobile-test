a16197dadfd6ea106737ac07416855e19a8855de
Nihal Ismail <nihalismailk@gmail.com>
2021-10-30 12:25:10 -0500

    seed & verify-seed init

--- nodejs/functions/index.js
+++ nodejs/functions/index.js
@@ -1,8 +1,23 @@
+//Objects init
 const functions = require('firebase-functions');
+const express = require('express');
+const app = express();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+//Routes
+//Get Seed
+app.get("/api/seed",(req,res)=>{
+    return res.status(200).send({
+        "seed": "",
+        "time": ""
+    });
+});
+
+//Verify Seed
+app.all("/api/verify-seed",(req,res)=>{
+    return res.status(200).send({
+        "seed": "",
+    });
+});
+
+//Export the app
+exports.app = functions.https.onRequest(app);
