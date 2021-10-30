b63dadc7578cefba2d902d71a0e71968efe623e0
Nihal Ismail <nihalismailk@gmail.com>
2021-10-30 13:42:00 -0500

    QR Code Library Added

--- flutter/lib/screens/qr_code_screen.dart
+++ flutter/lib/screens/qr_code_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 import 'package:superformula_scanner/screens/home_screen.dart';
 import 'package:superformula_scanner/screens/scanner_screen.dart';
 import 'package:superformula_scanner/widgets/action_button.dart';
@@ -20,6 +21,27 @@ class _QRCCodeScreenState extends State<QRCCodeScreen> {
     return Scaffold(
       appBar: AppBar(
         title: const Text("QR Code"),
+      ),
+      body: SizedBox(
+        width: double.infinity,
+        height: double.infinity,
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: ScreenUtil().setHeight(80)),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              QrImage(
+                data: "1234567890",
+                version: QrVersions.auto,
+                size: ScreenUtil().setHeight(300),
+              ),
+              SizedBox(
+                height: ScreenUtil().setHeight(48),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
