268501c539c475d486039ffd961c92d9ee3415c4
Nihal Ismail <nihalismailk@gmail.com>
2021-10-30 12:28:38 -0500

    seed funcion 100% completed

--- nodejs/functions/index.js
+++ nodejs/functions/index.js
@@ -3,12 +3,21 @@ const functions = require('firebase-functions');
 const express = require('express');
 const app = express();
 
+//Seed generator function
+const generateSeed = s => s.split('').reduce((a,b) => (((a << 5) - a) + b.charCodeAt(0))|0, 0);
+
+//Key generator
+var datetime = new Date();
+var key = datetime.getUTCHours().toString() + datetime.getUTCMinutes().toString();
+
 //Routes
 //Get Seed
 app.get("/api/seed",(req,res)=>{
+
+    //Generate a seed based on the current time
     return res.status(200).send({
-        "seed": "",
-        "time": ""
+        "seed": generateSeed(key),
+        "time": key
     });
 });
 
