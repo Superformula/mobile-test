386d221beb2f3a2a35c52842e0616f10342c2ed6
Juan Mayobre <juanmayobre@juans-mbp.lan>
2021-11-09 12:02:53 -0500

    [Fix] BlocListener empty

--- qr_generator/lib/screens/qr_details/qr_details.dart
+++ qr_generator/lib/screens/qr_details/qr_details.dart
@@ -53,66 +53,62 @@ class _QrDetailsState extends State<QrDetails> {
         ],
       ),
       body: SafeArea(
-        child: BlocListener(
-            bloc: _qrDetailsBloc,
-            listener: (context, state) {},
-            child: BlocBuilder(
-              bloc: _qrDetailsBloc,
-              builder: (context, state) {
-                if (state is QrDetailsError) {
-                  return ErrorView(
-                    appBar: true,
-                    error: state.error,
-                  );
-                }
-                if (state is QrDetailsLoaded) {
-                  if (state.offline && state.seedData == null) {
-                    return const OfflineView();
-                  }
-                  return Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: Center(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          offlineChip(state.offline),
-                          Card(
-                            child: QrImage(
-                              data: state.seedData.id,
-                              version: QrVersions.auto,
-                              size: MediaQuery.of(context).size.width / 1.2,
-                              gapless: false,
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          Visibility(
-                            //visible: state.,
-                            child: CountdownTimer(
-                              textStyle: _theme.textTheme.headline4,
-                              endTime: state
-                                  .seedData.expiresAt.millisecondsSinceEpoch,
-                              onEnd: () => _qrDetailsBloc.add(RefreshSeed()),
-                            ),
-                            replacement: Text(
-                              'Expired',
-                              style: _theme.textTheme.headline4
-                                  ?.copyWith(color: Colors.red),
-                            ),
-                          ),
-                        ],
+          child: BlocBuilder(
+        bloc: _qrDetailsBloc,
+        builder: (context, state) {
+          if (state is QrDetailsError) {
+            return ErrorView(
+              appBar: true,
+              error: state.error,
+            );
+          }
+          if (state is QrDetailsLoaded) {
+            if (state.offline && state.seedData == null) {
+              return const OfflineView();
+            }
+            return Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: Center(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    offlineChip(state.offline),
+                    Card(
+                      child: QrImage(
+                        data: state.seedData.id,
+                        version: QrVersions.auto,
+                        size: MediaQuery.of(context).size.width / 1.2,
+                        gapless: false,
                       ),
                     ),
-                  );
-                }
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
-              },
-            )),
-      ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Visibility(
+                      //visible: state.,
+                      child: CountdownTimer(
+                        textStyle: _theme.textTheme.headline4,
+                        endTime:
+                            state.seedData.expiresAt.millisecondsSinceEpoch,
+                        onEnd: () => _qrDetailsBloc.add(RefreshSeed()),
+                      ),
+                      replacement: Text(
+                        'Expired',
+                        style: _theme.textTheme.headline4
+                            ?.copyWith(color: Colors.red),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          }
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      )),
     );
   }
 
