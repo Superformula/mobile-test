6d18787337bf5728525b8635e6f29d68b4e6269c
Ted Landis <ted@atomicrobot.com>
2021-11-28 15:34:16 -0500

    add generated seed model

---
+++ qr-generator-app/lib/model/seed.g.dart
@@ -0,0 +1,12 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'seed.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Seed _$SeedFromJson(Map<String, dynamic> json) => Seed(
+      json['seed'] as String,
+      DateTime.parse(json['expires_at'] as String),
+    );
