6e493e0cbf8afd1f22f09eda12862cfe17f6967f
Ted Landis <ted@atomicrobot.com>
2021-11-28 21:01:40 -0500

    add immutable

--- qr-generator-app/lib/model/qr_data.dart
+++ qr-generator-app/lib/model/qr_data.dart
@@ -2,13 +2,14 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'qr_data.g.dart';
 
+@immutable
 @JsonSerializable(createToJson: false)
 class QrData {
   final String seed;
   @JsonKey(name: "expires_at")
   final DateTime expiresAt;
 
-  QrData(this.seed, this.expiresAt);
+  const QrData(this.seed, this.expiresAt);
 
   factory QrData.fromJson(final Map<String, dynamic> json) =>
       _$QrDataFromJson(json);
