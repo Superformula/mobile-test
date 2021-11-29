4db4d8a37410deab8b8b235c7e1414542e8d6382
Ted Landis <ted@atomicrobot.com>
2021-11-28 21:57:55 -0500

    added floating action buttons

--- qr-generator-app/lib/feature/home/home_page.dart
+++ qr-generator-app/lib/feature/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:qr_generator_app/app/routes.dart';
 
 class HomePage extends StatelessWidget {
@@ -10,22 +11,18 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("QR Generator App"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text("home"),
-            ElevatedButton.icon(
-                onPressed: () => Navigator.of(context).pushNamed(AppRoutes.displayQr),
-                icon: const Icon(Icons.qr_code),
-                label: const Text("display qr"),),
-            ElevatedButton.icon(
-                onPressed: () => Navigator.of(context).pushNamed(AppRoutes.scanQr),
-                icon: const Icon(Icons.qr_code),
-                label: const Text("scan qr"),)
-
-          ],
-        ),
+      floatingActionButton: SpeedDial(
+        animatedIcon: AnimatedIcons.menu_close,
+        children: [
+          SpeedDialChild(
+            child: const Icon(Icons.qr_code),
+            onTap: () => Navigator.of(context).pushNamed(AppRoutes.displayQr),
+          ),
+          SpeedDialChild(
+            child: const Icon(Icons.camera_alt_outlined),
+            onTap: () => Navigator.of(context).pushNamed(AppRoutes.scanQr),
+          )
+        ],
       ),
     );
   }
