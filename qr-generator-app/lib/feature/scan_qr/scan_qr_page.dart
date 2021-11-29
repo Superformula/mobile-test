bfe7f13d2d5236c78f890fbeb344b4632b55341b
Ted Landis <ted@atomicrobot.com>
2021-11-28 21:18:28 -0500

    added qr scanner

--- qr-generator-app/lib/feature/scan_qr/scan_qr_page.dart
+++ qr-generator-app/lib/feature/scan_qr/scan_qr_page.dart
@@ -1,8 +1,36 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class ScanQrPage extends StatelessWidget {
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+
+class ScanQrPage extends StatefulWidget {
   const ScanQrPage({Key? key}) : super(key: key);
+
+  @override
+  State<ScanQrPage> createState() => _ScanQrPageState();
+}
+
+class _ScanQrPageState extends State<ScanQrPage> {
+  final GlobalKey qrKey = GlobalKey(debugLabel: "scan qr page");
+  Barcode? result;
+  QRViewController? controller;
+
+  @override
+  void reassemble() {
+    super.reassemble();
+    if (Platform.isAndroid) {
+      controller?.pauseCamera();
+    } else if (Platform.isIOS) {
+      controller?.resumeCamera();
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller?.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,7 +38,32 @@ class ScanQrPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Scan QR"),
       ),
-      body: Container(),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 5,
+            child: QRView(
+              key: qrKey,
+              onQRViewCreated: _onQrViewCreated,
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Center(
+              child: (result != null)
+                  ? Text(
+                      "Barcode Type: ${describeEnum(result!.format)}   Data: ${result!.code}")
+                  : const Text("Scan a code"),
+            ),
+          )
+        ],
+      ),
     );
+  }
+
+  void _onQrViewCreated(QRViewController controller) {
+    this.controller = controller;
+    controller.scannedDataStream
+        .listen((scanData) => setState(() => result = scanData));
   }
 }
