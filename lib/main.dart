a2d235a94286ffb37fc4874147bdc0b90c36f56b
Hudson <devhudsoncontact@gmail.com>
2023-02-12 16:13:45 -0300

    Injecting dependencie on app initialize

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:superformula_test/core/resources/app_routes.dart';
+import 'package:superformula_test/core/resources/injector.dart';
 import 'package:superformula_test/core/theme/app_theme.dart';
 import 'package:superformula_test/view/pages/home_page.dart';
 
+import 'view/blocs/qr_code_history_bloc/qr_code_history_bloc.dart';
+
 void main() {
+  InitializeInjector.instance.inject();
   runApp(const App());
 }
 
@@ -12,11 +17,15 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: AppTheme.themeData,
-      home: const HomePage(),
-      routes: AppRoutes.routes,
+    return BlocProvider<QrCodeHistoryCubit>(
+      create: (context) => QrCodeHistoryCubit(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: AppTheme.themeData,
+        home: const HomePage(),
+        debugShowCheckedModeBanner: false,
+        routes: AppRoutes.routes,
+      ),
     );
   }
 }
