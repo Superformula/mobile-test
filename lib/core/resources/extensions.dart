2594c126bff63637f634fe82a8f92a868dad35a8
Hudson <devhudsoncontact@gmail.com>
2023-02-12 16:13:26 -0300

    Adding some extension classes to the project

--- lib/core/resources/extensions.dart
+++ lib/core/resources/extensions.dart
@@ -4,3 +4,24 @@ extension ThemeExtension on BuildContext {
   ThemeData get themeData => Theme.of(this);
   TextTheme get textTheme => themeData.textTheme;
 }
+
+extension SizeExtension on BuildContext {
+  double get screenHeight => MediaQuery.of(this).size.height;
+  double get screenWidth => MediaQuery.of(this).size.width;
+}
+
+extension SliverExtension on Widget {
+  Widget get toSliver => SliverToBoxAdapter(
+        child: this,
+      );
+}
+
+extension DateFormatExtension on DateTime {
+  String get toMMddYYYYHHmm {
+    return '${month.timePadLeft}/$day/$year - ${hour.timePadLeft}:${minute.timePadLeft}';
+  }
+}
+
+extension TimePadLeftExtension on int {
+  String get timePadLeft => toString().padLeft(2, '0');
+}
