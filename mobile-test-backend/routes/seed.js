e1a5dd072c8b3a78dedfc8d3ae0f52caa8977cb7
Leandro Forte <premiersoft-leandro.meneses@ailos.coop.br>
2024-02-08 15:20:59 -0300

    remove time zone offset from expires_at and add 1 minute from now

--- mobile-test-backend/routes/seed.js
+++ mobile-test-backend/routes/seed.js
@@ -3,11 +3,12 @@ const randomstring = require("randomstring");
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    const dateNowMilliseconds = Date.now();
-    const date = new Date(dateNowMilliseconds);
-    var formattedDate = date.toISOString().replace('Z', '')
+    const tzoffset = (new Date()).getTimezoneOffset() * 60000;
+    const localISOTime = (new Date(Date.now() - tzoffset));
+    localISOTime.setMinutes(localISOTime.getMinutes() + 1);
+    const expires_at = localISOTime.toISOString().slice(0, -1);
     const seed = randomstring.generate();
-    res.json({'seed': seed, 'expires_at': formattedDate});
+    res.json({'seed': seed, 'expires_at': expires_at});
 });
 
 module.exports = router;
